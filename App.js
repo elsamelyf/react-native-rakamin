61d0f4584221c3014d77e2e2627eebdfb11bccc0
Elsa Amelya Fitriany <elsamelyaf@example.com>
2024-07-29 15:13:23 +0700

    notes apps

---
+++ App.js
@@ -0,0 +1,62 @@
+import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import colors from './src/theme/colors';
+import { Ionicons } from '@expo/vector-icons';
+
+
+import NotesListScreen from './src/screens/NotesListScreens';
+import AddNoteScreen from './src/screens/AddNoteScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <NavigationContainer theme= {{ //navigation container sebagai parent screen untuk setting tema
+        colors: {
+          backgroundColor: colors.textColors.white,
+        }
+        }}>
+        <Stack.Navigator screenOptions={{ //sebagai parent untuk bagian di dalamnya
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: colors.textColors.white,
+          }
+        }}>
+          <Stack.Screen 
+            name="NotesListScreen" 
+            component={NotesListScreen}
+            options={({ navigation }) => ({
+              headerTitle: 'Notes',
+              headerRight: () => (
+                <Ionicons
+                  name='add-circle-sharp'
+                  size={24}
+                  color={colors.primary.blue}
+                  onPress={() => navigation.navigate('AddNoteScreen')}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen 
+            name="AddNoteScreen" 
+            component={AddNoteScreen}
+            options={({navigation}) => ({
+              headerTitle: 'Add New Note',
+              headerLeft: () => (
+                <Ionicons
+                  name= 'arrow-back-circle'
+                  size={24}
+                  color={colors.primary.blue}
+                  onPress={() => navigation.goBack()}
+             /> 
+            ),
+          })}
+        />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
